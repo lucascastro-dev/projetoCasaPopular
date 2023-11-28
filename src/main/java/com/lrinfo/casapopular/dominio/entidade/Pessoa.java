302de0eda648a202deedc0e2b89391aead33fbaa
Lucas Castro <lucas.ferreira@mjv.com.br>
2023-11-28 14:38:36 -0300

    Incluindo anotação de all args constructor para não precisar criar um construtor manualmente

--- src/main/java/com/lrinfo/casapopular/dominio/entidade/Pessoa.java
+++ src/main/java/com/lrinfo/casapopular/dominio/entidade/Pessoa.java
@@ -1,16 +1,13 @@
 package com.lrinfo.casapopular.dominio.entidade;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class Pessoa {
-    Double renda = 0.0;
-    Integer idade = 0;
-
-    public Pessoa(Double renda, Integer idade) {
-        this.renda = renda;
-        this.idade = idade;
-    }
+    Double renda;
+    Integer idade;
 }
